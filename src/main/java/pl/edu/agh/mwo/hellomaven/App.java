327d08995cdb0ef5ae01264b4d0296e3119ba37f
p0dkan <podkanowiczpiotr@gmail.com>
2025-04-06 14:42:15 +0200

    end of classes

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -1,17 +1,33 @@
 package pl.edu.agh.mwo.hellomaven;
 
+import com.indvd00m.ascii.render.Render;
+import com.indvd00m.ascii.render.api.ICanvas;
+import com.indvd00m.ascii.render.api.IContextBuilder;
+import com.indvd00m.ascii.render.api.IRender;
+import com.indvd00m.ascii.render.elements.PseudoText;
+
 /**
- * Simple main application
+ * Simple main application.
  */
 public class App {
 
+    public static final int widith = 120;
+    public static final int height = 20;
+    public static final int randomNumber = 4;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        IRender render = new Render();
+        IContextBuilder builder = render.newBuilder();
+        builder.width(widith).height(height);
+        builder.element(new PseudoText("Hello Maven!"));
+        ICanvas canvas = render.render(builder.build());
+        String s = canvas.getText();
+        System.out.println(s);
     }
-    
+
     public int getRandomNumber() {
         // chosen by fair dice roll.
         // guaranteed to be random
-        return 4;
+        return randomNumber;
     }
 }
